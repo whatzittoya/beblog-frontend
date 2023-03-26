ba0fd3d545f173f7954927f083f508f8abaf91b4
whatzittoya <whosendall.com>
2023-03-26 07:19:02 +0700

    ok

--- next.config.js
+++ next.config.js
@@ -8,7 +8,12 @@ const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     minimumCacheTTL: 6000,
-    domains: ['localhost', 'cms.payloadcms.com', 'stage.cms.payloadcms.com'],
+    domains: [
+      'localhost',
+      'cms.payloadcms.com',
+      'stage.cms.payloadcms.com',
+      'beblog-backend-production.up.railway.app',
+    ],
   },
   experimental: {
     appDir: true,
